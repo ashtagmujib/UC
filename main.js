ccd6fa49bce6d38372ecbb949dde90229ee04f9f
Abdul-Mujib <mujibajimoh@gmail.com>
2023-10-27 11:06:43 -0700

    removing dark mode

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const moon = document.getElementById('moon'),
-    sun = document.getElementById('sun')
-;
+// const moon = document.getElementById('moon'),
+//     sun = document.getElementById('sun')
+// ;
 
-moon.addEventListener('click', e => { 
-    document.querySelector('body').style.backgroundColor = '#000';
-    document.querySelector('.container').style.backgroundColor = '#000';
-    document.querySelector('p').style.color = '#fff';
+// moon.addEventListener('click', e => { 
+//     document.querySelector('body').style.backgroundColor = '#000';
+//     document.querySelector('.container').style.backgroundColor = '#000';
+//     document.querySelector('p').style.color = '#fff';
 
-    moon.style.display = 'none';
-    sun.style.display = 'block';
-})
+//     moon.style.display = 'none';
+//     sun.style.display = 'block';
+// })
 
-sun.addEventListener('click', e=> {
-    document.querySelector('body').style.backgroundColor = '#fff';
-    document.querySelector('.container').style.backgroundColor = '#fff';
-    document.querySelector('p').style.color = '#000';
+// sun.addEventListener('click', e=> {
+//     document.querySelector('body').style.backgroundColor = '#fff';
+//     document.querySelector('.container').style.backgroundColor = '#fff';
+//     document.querySelector('p').style.color = '#000';
     
-    moon.style.display = 'block';
-    sun.style.display = 'none';
-})
+//     moon.style.display = 'block';
+//     sun.style.display = 'none';
+// })
 
 
 
