001fc1d6b6b947b0fb5d923763e17d8c050b0e15
Abdul-Mujib <mujibajimoh@gmail.com>
2024-03-29 12:02:54 -0700

    code review

--- main.js
+++ main.js
@@ -221,7 +221,7 @@ const calculatetotalUnit = () => {
         unitSum += unitNum[i] 
     }
 
-    // display total unit
+    // display total unitw
     const totalUnit = document.querySelector('#unit-Total')
     totalUnit.innerText = unitSum;
 
@@ -387,12 +387,12 @@ calculate.addEventListener('click', () => {
             }
 
             
-            else if(msgDts >= 1.0) {
-                classMsg.innerText = 'you had a pass, try harder next time, you gat this';
-            }
+            // else if(msgDts >= 1.0) {
+            //     classMsg.innerText = 'you had a pass, try harder next time, you gat this';
+            // }
 
     
-            else if(msgDts < 1.0) {
+            else if(msgDts < 1.5) {
                 classMsg.innerText = 'omo it looks like you failed, try harder next time, you gat this';
             }
             
